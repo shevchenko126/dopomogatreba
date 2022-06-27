33591177dd20005761a049e5fc411ffc3d1274c7
Alex Bond <nfs2008@windowslive.com>
2022-06-27 16:05:49 +0300

    make model blog

--- backend/content/models.py
+++ backend/content/models.py
@@ -3,7 +3,33 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField()
+    title = models.CharField(
+        verbose_name="Назва", blank=True, null=True
+        )
+    def __str__(self):
+        return self.title
+    
+    def save(self):
+        super().save()
+    class Meta:
+        verbose_name = "Блог"
+        verbose_name_plural = "Блоги"
 
 class BlogCategory(models.Model):
-    title = models.CharField() 
+    title = models.CharField(
+        verbose_name="Назва", blank=True, null=True
+        )
+    
+    blog = models.ForeignKey(
+        Blog, verbose_name="Назва блогу", null=True, blank=True
+        )
+
+    def __str__(self):
+        return self.title
+    
+    def save(self):
+        super().save()
+
+    class Meta:
+        verbose_name = "Категорія"
+        verbose_name_plural = "Категорії"
