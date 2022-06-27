6c1a8628050d1edd2d2e23b3bbd775595c500b5b
Alex Bond <nfs2008@windowslive.com>
2022-06-27 17:23:15 +0300

    models max_length and on_delete

--- backend/content/models.py
+++ backend/content/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(
-        verbose_name="Назва", blank=True, null=True
+        verbose_name="Назва", blank=True, null=True, max_length=30
         )
     def __str__(self):
         return self.title
@@ -17,11 +17,11 @@ class Blog(models.Model):
 
 class BlogCategory(models.Model):
     title = models.CharField(
-        verbose_name="Назва", blank=True, null=True
+        verbose_name="Назва", blank=True, null=True, max_length=30
         )
     
     blog = models.ForeignKey(
-        Blog, verbose_name="Назва блогу", null=True, blank=True
+        Blog, verbose_name="Назва блогу", null=True, blank=True, on_delete=models.SET_NULL
         )
 
     def __str__(self):
--- backend/dopomogatreba/settings.py
+++ backend/dopomogatreba/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ub@!xc6qap-#(4&32x5*mq*g1t#of65g5q3@tn%xy@wgkm5tc1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'content'
 ]
 
 MIDDLEWARE = [
