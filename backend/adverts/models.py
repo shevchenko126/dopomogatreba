db21f01fe867b0b0f135e1a7dca68111fe4da225
Dmitry Siemens <d.siemens71@gmail.com>
2022-06-29 15:44:54 +0300

    исправление urls.py

--- backend/adverts/models.py
+++ backend/adverts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Advert(models.Model):
-    titel = models.CharField(verbose_name = 'title', max_length=150, db_index=True)
-    image = models.FieldFile(upload_to='uploads/', max_length=254)
+    titel = models.CharField(verbose_name = 'title', max_length=150, db_index=True, blank=True, null=True)
+    image = models.FieldFile(upload_to='uploads/', max_length=254, default="images/default.jpg", blank=True, null=True)
     description  = models.TextField(blank = True, null= True, db_index=True)
 
     def __str__(self):
