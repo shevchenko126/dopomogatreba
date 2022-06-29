9117a9e541a11315bdc82b38b9436fdab5c42dbf
Dmitry Siemens <d.siemens71@gmail.com>
2022-06-29 17:13:20 +0300

    create models AdvertResponse

--- backend/adverts/admin.py
+++ backend/adverts/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from adverts.models import Advert
+from adverts.models import AdvertResponse
 
 admin.site.register(Advert)
+admin.site.register(AdvertResponse)
--- backend/adverts/models.py
+++ backend/adverts/models.py
@@ -1,12 +1,46 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User
 
 class Advert(models.Model):
-    titel = models.CharField(verbose_name = 'title', max_length=150, db_index=True, blank=True, null=True)
-    image = models.FieldFile(upload_to='uploads/', max_length=254, default="images/default.jpg", blank=True, null=True)
-    description  = models.TextField(blank = True, null= True, db_index=True)
+    title = models.CharField(verbose_name = 'title', 
+                             max_length=150, 
+                             db_index=True, 
+                             blank=True, 
+                             null=True)
+    image = models.FieldFile(upload_to='uploads/', 
+                             max_length=254, 
+                             default="images/default.jpg", 
+                             blank=True, 
+                             null=True)
+    description  = models.TextField(blank = True, 
+                                    null= True, 
+                                    db_index=True)
 
     def __str__(self):
-        return self.titel
+        return self.title
+
+    def seve(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    class Mena:
+        verbose_name = 'Title'
+        verbose_name_plural = 'Titles'
+
+class AdvertResponse(models.Model):
+    title = models.CharField(verbose_name = 'title', 
+                             max_length=250, 
+                             db_index=True, 
+                             blank=True, 
+                             null=True)
+    user = models.ForeignKey(User, blank = True, null=True, 
+                                   on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return self.title
+
+    def seve(self, *args, **kwargs):
+        super().save(*args, **kwargs)
 
     class Mena:
         verbose_name = 'Title'
