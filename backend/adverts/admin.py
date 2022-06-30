4f796c3ddc9add7f0a6ec01d5345facc9933af74
oleksii.kormilets <oleksii.kormilets@stryker.com>
2022-06-30 16:25:26 +0300

    comm

--- backend/adverts/admin.py
+++ backend/adverts/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Advert, AdvertCategory, AdvertResponse, AdvertFeature
 
+
 admin.site.register(Advert)
 admin.site.register(AdvertCategory)
 admin.site.register(AdvertResponse)
-
 admin.site.register(AdvertFeature)
-
