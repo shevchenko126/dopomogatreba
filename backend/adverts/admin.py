e93d483a9334d1626c48f28db93137670b8073e4
oleksii.kormilets <oleksii.kormilets@stryker.com>
2022-06-30 15:08:32 +0300

    модель AdvertFeature

--- backend/adverts/admin.py
+++ backend/adverts/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Advert, AdvertCategory, AdvertResponse
+from .models import Advert, AdvertCategory, AdvertResponse, AdvertFeature
 
 
 admin.site.register(Advert)
 admin.site.register(AdvertCategory)
 admin.site.register(AdvertResponse)
+admin.site.register(AdvertFeature)
+
