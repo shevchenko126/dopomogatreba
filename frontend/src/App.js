9102920c75b74eef27fb0d7b9923c4c83409ac0f
m1nusplus <v.gaganenko@gmail.com>
2022-07-01 12:50:54 +0300

    Add SignInFeature block

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+//import './App.css';
 import FreshRecommendedAds from './components/contentBlocks/FreshRecommendedAds';
-
+import SignInFeature from './components/contentBlocks/08_SignIn_Feature';
 function App() {
   return (
     <div className="App">
       <FreshRecommendedAds />
+      <SignInFeature />
     </div>
   );
 }
