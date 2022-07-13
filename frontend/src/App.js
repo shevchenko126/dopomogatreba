44ab3170bf23af81bce37376f5464821eae28a36
vmelekhova <vmelekhova@stud.khpi.ucode-connect.study>
2022-07-13 21:05:23 +0300

    added: structure for modal window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 //import './App.css';
 import FreshRecommendedAds from './components/contentBlocks/FreshRecommendedAds';
 import SignInFeature from './components/contentBlocks/08_SignIn_Feature';
+import PaymentDetails from './components/contentBlocks/PaymentDetails';
+
 function App() {
   return (
     <div className="App">
-      <FreshRecommendedAds />
-      <SignInFeature />
+      {/* <FreshRecommendedAds /> */}
+      <PaymentDetails />
+      {/* <SignInFeature /> */}
     </div>
   );
 }
---
+++ frontend/src/components/contentBlocks/PaymentDetails/index.js
@@ -0,0 +1,47 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './index.css'
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+
+const PaymentDetails = () => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="primary" onClick={handleShow}>
+                open modal
+            </Button>
+
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop="static"
+                keyboard={false}
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal title</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    I will not close if you click outside me. Don't even try to press
+                    escape key.
+                    
+                    отут будет все 
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary">Understood</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default PaymentDetails
