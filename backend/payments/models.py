92ff6a298fa5d8f85cd7c6d60b7ab4cb4d41f8f4
Dmitry Siemens <d.siemens71@gmail.com>
2022-07-13 17:58:20 +0300

    Change modelss

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -6,8 +6,9 @@ from adverts.models import Advert
 class Payment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     advent = models.ForeignKey(Advert,on_delete=models.CASCADE, blank=True, null=True)
-    amount = models.DecimalField(verbose_name='Amount', max_digits=9, decimal_places=2, blank=True, null=True)
-    is_paid = models.BooleanField(default=True)
+    amount = models.DecimalField(verbose_name='Amount', max_digits=9, decimal_places=2,
+                                 blank=True, null=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return self.amount
