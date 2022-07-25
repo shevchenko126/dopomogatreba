402e6d0fe7c972ccae2cee8d928d0667277eb598
oleksii.kormilets <oleksii.kormilets@stryker.com>
2022-07-25 16:35:42 +0300

    Сделать авторизацию

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 # Create your models here.
@@ -29,3 +31,8 @@ class Address(models.Model):
     class Meta:
         verbose_name = "Address"
         verbose_name_plural = "Addresses" 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
