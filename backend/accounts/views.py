d55cfe33e16d5559c17a5dc492b2f22b0205d18b
sgureev <gureev.serg@gmail.com>
2022-08-03 13:24:58 +0300

    Adverts By User Token

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,65 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< Updated upstream
+=======
+class CustomAuthData(ObtainAuthToken):
+    queryset = User.objects.all()
+    # def post(self,request, *args, **kwargs):
+    #     user_data = {
+    #         "username": request.data['username'.lower],
+    #         "password": request.data['password']
+    #     }
+        
+    #     try:
+    #         user = authenticate(**user_data)
+    #         resp = {
+    #             "success":True,
+    #             "token": Token.objects.filter(user=user).first().key,
+    #             "user_id": user.id,
+    #             "user_email": user.email,
+    #         }
+    #     except:
+    #         resp = {
+    #             "success":False,
+    #             "token": "",
+    #             "user_id": 0,
+    #             "user_email": "",
+    #         }
+    #     return Response(resp)
+    def post(self, request, *args, **kwargs):
+            serializer = self.serializer_class(data=request.data,
+                                       context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key,
+                'user_id': user.pk,
+                'email': user.email
+        })
+    
+    
+    def get(self,request, *args, **kwargs):
+        token = request.data.get('Authorization')
+        user = Token.objects.get(key=token).user 
+        try:
+               
+            user_id = user.pk
+            user_email = user.email
+            print(str(token))
+        except Token.DoesNotExist:
+            user_id = 0
+            user_email = ""
+            print(str(token))
+            return Response({
+                "token": token,
+                "user_id": 0,
+                "user_email": "",
+            })
+        return Response({
+            "token": token,
+            "user_id": user.pk,
+            "user_email": user.email,
+        })
+>>>>>>> Stashed changes
--- backend/adverts/views.py
+++ backend/adverts/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+<<<<<<< Updated upstream
 
 # Create your views here.
+=======
+from django.http import HttpResponse, JsonResponse
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.views import View
+from adverts.models import Advert
+
+def my_view(request):
+    if request.method == 'get':
+        return HttpResponse('result')
+
+class ViewAllAdverts(View):
+    def get(self, request):
+
+        title = request.GET.get('title', False)
+        adverts = Advert.objects.all()
+        if title:
+            adverts = adverts.filter(title=title)
+
+        adverts_data = []
+        for advert in adverts:
+            adverts_data.append({
+                "title":advert.title
+            })
+        return render(request, 'adverts/adverts.html', {"adverts":adverts_data, "show":False })
+        # return JsonResponse({"data":adverts_data})
+
+    def post(self, request):
+        name = request.POST.get('name', '')
+
+        print(name)
+        print(request.__dict__)
+        return HttpResponse(name)
+
+class ViewUserAdverts(View):
+    def get(self,request, *args, **kwargs):
+        token = self.request.headers.get('token', '')
+        try:
+            user = Token.objects.get(key=token).user   
+            user_id = user.pk
+        except Token.DoesNotExist:
+            user_id = 0
+        
+        adverts_data = []
+
+        if user_id>0:
+            adverts = Advert.objects.all()
+            adverts = adverts.filter(author_id=user_id)
+            adverts_data = []
+            for advert in adverts:
+                adverts_data.append({
+                    "title":advert.title,
+                    'image':advert.image,
+                    'description':advert.description,
+                    'author':advert.author, 
+                })
+        return render(request, 'adverts/adverts.html', {"adverts":adverts_data, "show":True })
+        # return JsonResponse({"data":adverts_data})
+>>>>>>> Stashed changes
